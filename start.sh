0a0970d5ad0b2302ffd53fe2558e3283db57fdb3
Jake Lever <jake.lever@gmail.com>
2025-12-16 17:36:31 +0000

    Poke apps to start them

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ shiny-server &
 # Wait a few seconds to make sure Shiny starts
 sleep 3
 
+# Preload specific apps
+echo "Warming up Shiny apps..."
+curl -s http://localhost:3838/cancermine/ > /dev/null
+curl -s http://localhost:3838/civicmine/ > /dev/null
+
 # Start nginx in foreground (so Docker health checks work)
 echo "Starting nginx..."
 nginx -g "daemon off;"
